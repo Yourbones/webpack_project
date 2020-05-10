4ea00711af9f939b8c4c6c2ba839dbac132c3a85
许浩星 <xuhaoxing9@163.com>
2020-05-10 17:37:25 +0800

    feat: 新建文件src/index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+class Father {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Son extends Father {
+    constructor(name, age) {
+        super(name);
+        this.age = age;
+    }
+}
+const D = new Son('Fly', 15);
+console.log('D', D);
