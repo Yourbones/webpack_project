19329f01cbaece0b3f7dbbb76d887df68bb676f6
许浩星 <xuhaoxing9@163.com>
2020-05-11 23:13:46 +0800

    feat: 新建mock/mocker.js用于模拟接口返回数据

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.less';
 
 // 添加这段代码，避免了更改代码整个页面都刷新
-if(module && module.hot) {
+if (module && module.hot) {
     module.hot.accept();
 }
 
@@ -20,11 +20,26 @@ class Son extends Father {
 const D = new Son('Fly', 15);
 console.log('D', D);
 
-document.getElementsByTagName('button')[0].onclick = function() {
+document.getElementsByTagName('button')[0].onclick = function () {
     import('../public/js/handle.js').then(fn => fn.default());
 }
 
-fetch("/api/user")
+fetch("/user")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+fetch("/login/account", {
+    method: "POST",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        username: "admin",
+        password: "888888"
+    })
+})
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(err => console.log(err));
