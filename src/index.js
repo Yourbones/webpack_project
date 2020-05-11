c62bd6cb11edefd17791d149335b78623db6a5ca
许浩星 <xuhaoxing9@163.com>
2020-05-11 11:18:39 +0800

    feat: 避免热更新造成整个页面刷新

--- src/index.js
+++ src/index.js
@@ -17,3 +17,8 @@ console.log('D', D);
 document.getElementsByTagName('button')[0].onclick = function() {
     import('../public/js/handle.js').then(fn => fn.default());
 }
+
+// 添加这段代码，避免了更改代码整个页面都刷新
+if(module && module.hot) {
+    module.hot.accept();
+}
