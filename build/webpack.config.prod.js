b3cfec026fa6a69a487aa4573f2ff7414461837e
许浩星 <xuhaoxing9@163.com>
2020-05-12 09:44:37 +0800

    setting: 配置插件(量化plugin和loader花费时间)

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,9 +1,13 @@
 // 定义生产环境的配置
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const smp = new SpeedMeasurePlugin();
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.config.base');
-
-module.exports = merge(baseWebpackConfig, {
+ 
+const config = merge(baseWebpackConfig, {
     mode: 'production',
     // ...其它的配置
     devtool: 'hidden-source-map'
 })
+
+module.exports = smp.wrap(config);
